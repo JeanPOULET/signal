415b98a38b5381daf4a402f1be70dd308825e9d8
Nocturne-hub <jeremy-du25@hotmail.fr>
2020-01-30 16:25:05 +0100

    c'est l'heure

--- Signal.h
+++ Signal.h
@@ -7,86 +7,119 @@
 
 namespace sig {
 
-  /*
-   * Combiner that discards all values
-   */
-  class DiscardCombiner {
-  public:
-    using result_type = /* implementation defined */;
+	/*
+	* Combiner that discards all values
+	*/
+	class DiscardCombiner {
+		public:
+		using result_type = void;
 
-    template<typename U>
-    void combine(const U&) {
-    }
-
-    result_type result() {
-    }
-  };
-
-  /*
-   * Combiner that keeps only the last value
-   */
-  template<typename T>
-  class LastCombiner {
-  public:
-    using result_type = /* implementation defined */;
-
-    template<typename U>
-    void combine(const U& item) {
-    }
-
-    result_type result() {
-    }
-  };
-
-  /*
-   * Combiner that keeps all values in a vector
-   */
-  template<typename T>
-  class VectorCombiner {
-  public:
-    using result_type = /* implementation defined */;
-
-    template<typename U>
-    void combine(const U& item) {
-    }
-
-    result_type result() {
-    }
-  };
-
-  /*
-   * A signal
-   */
-  template<typename Signature, typename Combiner = DiscardCombiner>
-  class Signal {
-  public:
-    // result of combiner
-    using result_type = void;
-    
-    // constructor
-    Signal(Combiner combiner = Combiner()) {
-		std::vector<std::function<combiner>> save;
-    }
-
-    // connect the function to the signal and returns an id
-    template<typename Func>
-    std::size_t connectSlot(Func callback) {
-		save.push_back(callback);
-    }
-
-    // disconnect the function represented by the id
-    void disconnectSlot(std::size_t id) {
-		save.erase(save.begin()+id);
-    }
-
-    // emit a signal, call all the slots
-    result_type emitSignal(int args) {
-		for(size_t i = 0; i < save.size(); i++){
-			save[i](args);
+		template<typename U>
+		void combine(const U&) {
+			return;
 		}
-		return 
-    }
-  };
+
+		result_type result() {
+			return ;
+		}
+	};
+
+	/*
+	* Combiner that keeps only the last value
+	*/
+	template<typename T>
+	class LastCombiner {
+		public:
+		using result_type = T;
+
+		template<typename U>
+		void combine(const U& item) {
+
+		}
+
+		result_type result() {
+			
+		}
+	};
+
+	/*
+	* Combiner that keeps all values in a vector
+	*/
+	template<typename T>
+	class VectorCombiner {
+		public:
+		using result_type = T;
+
+		template<typename U>
+		void combine(const U& item) {
+
+		}
+
+		result_type result() {
+
+		}
+	};
+	template< class,class >
+	class Signal;
+	/*
+	* A signal
+	*/
+	template<typename Signature, typename Combiner, class... Args>
+	class Signal<Signature(Args...),Combiner> {
+		
+		public:
+		// result of combiner
+		using result_type = Signature;
+		Combiner combin;
+		std::vector<std::function<Signature(Args...)>> save;
+
+		// constructor
+		Signal(Combiner combiner = DiscardCombiner()) {
+			combin = combiner;
+			//std::vector<std::function<Signature(Args)>> save;
+		}
+
+		// connect the function to the signal and returns an id
+		template<typename Func>
+		std::size_t connectSlot(Func callback) {
+			save.push_back(callback);
+			return save.size();
+		}
+
+		// disconnect the function represented by the id
+		void disconnectSlot(std::size_t id) {
+			save.erase(save.begin()+id);
+		}
+
+		// emit a signal, call all the slots
+		result_type emitSignal(int args) {
+			int resType =-1;
+			switch(combin){
+				case DiscardCombiner :
+					resType = 0;
+					void res;
+				break;
+				case VectorCombiner :
+					resType = 1;
+					std::vector<result_type> res;
+				break;
+				default :
+					resType = 2;
+					result_type res;
+				break;
+			}
+			for(size_t i = 0; i < save.size(); i++){
+				if(resType ==1){
+					res.push_back(save[i](args));
+				}else if (resType ==2){
+					res = save[i](args);
+				}else{
+					save[i](args);
+				}
+			}
+			return res;
+		}
+  	};
 
 }
 
--- testSignal.cc
+++ testSignal.cc
@@ -4,18 +4,23 @@
 
 #include <gtest/gtest.h>
 
-void printou(int arg){
-  printf("CC CV LM : %d",arg);
+int printou(char arg){
+  	printf("CC CV LM : %d\n",arg);
+}
+
+void squeezie(char arg){
+ 	std::cout<<"SAlut à tous c'est SQU"<<arg<<"ZIE j'espère que vous aller mal !!!\n";
 }
 
 TEST(sigTest, firstTest){
-  	sig::Signal<void(int)> sig;
+  	sig::Signal<void(char),sig::DiscardCombiner> sig;
   	sig.connectSlot(printou);
-	sig.emitSignal(1);
+    sig.connectSlot(squeezie);
+	sig.emitSignal('b');
 
 }
 
 int main(int argc, char* argv[]) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  	::testing::InitGoogleTest(&argc, argv);
+  	return RUN_ALL_TESTS();
 }
